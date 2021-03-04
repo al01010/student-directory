9fc1cacd5409e2f91c12d2a39cdc4529cb658f04
Alice Lui <aytlui@gmail.com>
2021-03-04 15:57:09 +0000

    hash added

--- directory.rb
+++ directory.rb
@@ -1,27 +1,29 @@
 # students in an array
-students = ["Dr. Hannibal Lector", 
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West", 
-  "Terminator",
-  "Freddie Krueger",
-  "The Joker",
-  "Joffery Baratheon",
-  "Norman Bates"
+students = [
+  {name: "Dr. Hannibal Lector", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddie Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffery Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
+
 def print_header
   puts "The Students of Villains Academy"
   puts "---------------"
 end
 
-def print(names)
-  names.each {|student| puts student}
+def print(students)
+  students.each {|student| puts "student: #{student[:name]} (#{student[:cohort]} cohort)"}
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.length} great students"
 end
 
 print_header
